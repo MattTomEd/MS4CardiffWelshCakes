12bfb2ca13e33f45b83512ea958f895218f1c83a
Matthew Edwards <m.t.edwards410@gmail.com>
2022-01-30 23:38:17 +0000

    create blog urls.py, add to project levels urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """
+    Return all posts that have Published status. Order chronologically.
+    """
+    queryset = Post.objects.filter(status=1).order_by('-created_at')
+    template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
