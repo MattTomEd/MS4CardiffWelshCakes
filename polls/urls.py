cc07d1101f9e9172540679bede4e94e5d18c5df6
Matthew Edwards <m.t.edwards410@gmail.com>
2022-01-31 08:57:36 +0000

    adapt template from base website, adjust css so poll fits within page

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.polls, name='polls'),
     path('<int:question_id>/', views.detail, name='detail'),
