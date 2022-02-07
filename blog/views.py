304dac2d1f6c6a3a08334fe276b8debf4427cb8d
Matthew Edwards <m.t.edwards410@gmail.com>
2022-02-07 22:26:05 +0000

    Add comment system views, models, admin

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,30 @@ class post_list(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_at')
     template_name = 'index.html'
 
-class post_detail(generic.DetailView):
+class post_content(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'post_content.html'
+    
+from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+
+def post_detail(request, slug):
+    template_name = 'post_content.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
